6904aa1f105250212a3306314794806b45131c33
Johnny <johnlabedzki@gmail.com>
2024-03-16 10:36:25 -0700

    add initial blockchain class

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class BlockChain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block():
+        pass
+
+    def new_transaction():
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block():
+        pass
